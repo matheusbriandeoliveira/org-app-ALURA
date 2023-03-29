624dba587ff7382ec6a6ab37d058b6dd32ef57d4
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-03-29 14:35:58 -0300

    componente

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,25 +1,28 @@
 import React from "react"; 
-import { StyleSheet, Style, Image, Dimensions, Text, View } from "react-native";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+
+import Texto from '../telas/componentes/Texto.js';
 
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
+
 const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhes da cesta</Text>
+        <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.Fazenda}>
                 <Image source={logo} style={estilos.imageFazenda} />
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>                
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>                
             </View>
-            <Text style={estilos.descricao}>Uma cesta com produtos selecionados
+            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados
                 cuidadosamente da fazendo direto para
-                sua cozinha</Text>
-            <Text style={estilos.preco}>R$ 40,00</Text>
+                sua cozinha</Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
     </>
 }
@@ -49,7 +52,7 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: "MontserratBold",
+        fontWeight: "bold",
     },
 
     Fazenda: {
@@ -68,7 +71,6 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
-        fontFamily: "MontserratRegular",
     },
     descricao: {
         color: "#A3A3A3",
