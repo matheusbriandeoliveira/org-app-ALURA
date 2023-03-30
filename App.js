7ac89880669287ed93a581806a9451fc413ad5ad
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-03-30 17:59:32 -0300

    botao

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import { StatusBar, SafeAreaView, View } from 'react-native';
+
+
 import {
   useFonts,
   Montserrat_400Regular,
   Montserrat_700Bold
 } from '@expo-google-fonts/montserrat';
+import AppLoading from "expo-app-loading";
 
-import Cesta from './src/telas/Cesta';
+import Cesta from './src/telas/Cesta/Index';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -14,14 +19,14 @@ export default function App() {
   })
 
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
