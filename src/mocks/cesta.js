2e515624b84c59d5739c251e05ef0797d1c0cbd5
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-03-30 19:38:20 -0300

    lista

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,10 @@
 import logo from '../../assets/logo.png';
+import tomate from '../../assets/tomate.png';
+import brocolis from '../../assets/Brócolis.png';
+import batata from '../../assets/Batata.png';
+import pepino from '../../assets/Pepino.png';
+import abobora from '../../assets/Abóbora.png';
+
 
 const cesta = {
   topo: {
@@ -11,6 +17,31 @@ const cesta = {
     descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
     preco: "R$ 40,00",
     botao: "Comprar",
+    itens: {
+      titulo: "Itens da cesta",
+      lista: [
+        {
+          nome: "Tomate",
+          imagem: tomate, 
+        },
+        {
+          nome: "Brócolis",
+          imagem: brocolis, 
+        },
+        {
+          nome: "Batata",
+          imagem: batata, 
+        },
+        {
+          nome: "Pepino",
+          imagem: pepino, 
+        },
+        {
+          nome: "Abóbora",
+          imagem: abobora, 
+        }
+      ]
+    }
   }
 }
 
--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 import Texto from '../../../componentes/Texto';
+import Botao from '../../../componentes/Botao';
 
 export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
   return <>
@@ -9,13 +10,15 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
     <View style={estilos.fazenda}>
       <Image source={logoFazenda} style={estilos.imagemFazenda} />
       <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
+      
     </View>
+    
     <Texto style={estilos.descricao}>{ descricao }</Texto>
       <Texto style={estilos.preco}>{preco}</Texto>
 
-      <TouchableOpacity style={estilos.botao} onPress={() => {}}>
-          <Texto style={estilos.textoBotao}>{ botao }</Texto>
-      </TouchableOpacity>
+    <Botao texto={botao} style={estilos.botao} onPress={() => {}} />
+
+
   </>
 }
 
@@ -52,17 +55,4 @@ const estilos = StyleSheet.create({
     marginTop: 8,
     },
   
-    botao: {
-        marginTop: 16,
-        backgroundColor: "#2A9F85",
-        paddingVertical: 16,
-        borderRadius: 6,
-  },
-    textoBotao: {
-        textAlign: "center",
-        color: "#ffffff",
-        fontSize: 16,
-        lineHeight: 26,
-        fontWeight: "bold",
-  },
 })
